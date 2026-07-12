414a363dcad88b0c7a0260ef610103c391b4d9d5
heitor <heitorjaskojogos@gmail.com>
2026-07-11 23:19:26 -0300

    Delete sync_sws_colors.bat

--- sync_sws_colors.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Sincroniza as cores do AiNOMEATOR com o SWS Auto Color.
-cd /d "%~dp0"
-if exist venv\Scripts\activate.bat (
-    call venv\Scripts\activate.bat
-)
-python src\sync_sws_colors.py
